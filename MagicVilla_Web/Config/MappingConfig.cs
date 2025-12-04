96d371c97b1b29fe9be79c0ba82cffba8830666e
DevEmad002 <eatieh@ejada.com>
2025-12-04 11:36:36 +0300

    add SD class inside utility Class Libarary cinfigure Auto mapper in Web Project modifiey port numbers

---
+++ MagicVilla_Web/Config/MappingConfig.cs
@@ -0,0 +1,25 @@
+﻿
+
+using AutoMapper;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Models.Dto.VillaNumber;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.Dto.Villa;
+
+namespace MagicVilla_Web.Config
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO , VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO , VillaNumberUpdateDTO>().ReverseMap();
+
+        }
+    }
+}
